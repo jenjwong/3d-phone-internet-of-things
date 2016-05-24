6634b720235aa1d4d66942987d39ca16262b3aae
Hugo Nordell <hugo@flowfeed.co>
2016-05-23 22:35:05 -0700

    DO issues.

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const path = require('path'),
   io = require('socket.io')(server);
 
 // Server global acc, vel, dist variables
-// var aX = [0], aY = [0], aZ = [0],
-//   vX = [0], vY = [0], vZ = [0],
-//   sX = [0], sY = [0], sZ = [0];
 var aX = 0, aY = 0, aZ = 0,
   vX = 0, vY = 0, vZ = 0,
   sX = 0, sY = 0, sZ = 0;
@@ -26,9 +23,6 @@ io.on('connection', function ( socket ) {
     // if (aX.length >= 100) chopArrays(100);
     
     // Store acc data
-    // aX.push(data.accelerometer.x);
-    // aY.push(data.accelerometer.y);
-    // aZ.push(data.accelerometer.z);
     aX = data.accelerometer.x;
     aY = data.accelerometer.y;
     aZ = data.accelerometer.z;
@@ -60,6 +54,7 @@ io.on('connection', function ( socket ) {
   
   socket.on('disconnect', function() {
     console.log("Client disconnected.");
+    socket.broadcast.emit('disconnet', {});
   });
 });
 server.listen(port, function() {
@@ -77,31 +72,4 @@ function displacement3D(dt, steps) {
     sY += sY + dt*vY;
     sZ += sZ + dt*vZ;
   }
-}
-// function displacement3D(dt, steps) {
-//   for (var i = 1; i < steps + 1; i++) {
-//     // Update velocities
-//     vX.push( vX[i-1] +  dt*aX[i-1] );
-//     vY.push( vY[i-1] +  dt*aY[i-1] );
-//     vZ.push( vZ[i-1] +  dt*aZ[i-1] );
-//     
-//     // Update displacements
-//     sX.push( sX[i-1] + dt*vX[i-1] );
-//     sY.push( sY[i-1] + dt*vY[i-1] );
-//     sZ.push( sZ[i-1] + dt*vZ[i-1] );
-//     
-//     console.log(vX.length);
-//   }
-// }
-// Hack to keep performance on server
-function chopArrays(limit) {
-  aX = aX.slice(limit, aX.length);
-  aY = aY.slice(limit, aY.length);
-  aZ = aZ.slice(limit, aZ.length);
-  vX = vX.slice(limit, vX.length);
-  vY = vY.slice(limit, vY.length);
-  vZ = vZ.slice(limit, vZ.length);
-  sX = sX.slice(limit, sX.length);
-  sY = sY.slice(limit, sY.length);
-  sZ = sZ.slice(limit, sZ.length);
 }
